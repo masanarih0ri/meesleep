5fb634ab42d5dcd66369615f4a55e9d0f3be3339
masanarih0ri <masanarihoridevelop@gmail.com>
2022-06-14 09:50:10 +0900

    add spec for login status and logout status

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,15 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  let!(:user) { create(:user) }
-  before { sign_in user }
 
   describe 'GET /users/edit' do
-    subject { get edit_user_registration_path }
+    let!(:user) { create(:user) }
+    context 'when the user is logged in' do
+      before { sign_in user }
 
-    it 'renders user edit page' do
-      subject
-      expect(response).to have_http_status(:ok)
+      subject { get edit_user_registration_path }
+
+      it 'renders user edit page' do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when the user is not logged in' do
+      subject { get edit_user_registration_path }
+
+      it 'should redirect to user sign-in page' do
+        subject
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 end
