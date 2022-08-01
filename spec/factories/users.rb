5e30503135a71e5b50a8dad837e7ffd7de8d2276
masanarih0ri <masanarihoridevelop@gmail.com>
2022-08-01 21:23:27 +0900

    remove unnecessary code

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@
 
 FactoryBot.define do
   factory :user do
-    # Faker::Internet.password(min_length: 8)
     email { 'testuser1@example.com' }
     password { 'testuser1' }
     name { 'testuser1' }
