13509ea4f4ba2aaa371243802c76d014e42b1eab
masanarih0ri <masanarihoridevelop@gmail.com>
2022-08-07 22:39:41 +0900

    add validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,9 @@
 class Answer < ApplicationRecord
   belongs_to :log
   belongs_to :question
+
+  validates :log_id, presence: true
+  validates :question_id, presence: true
+  validates :is_good_habit, inclusion: { in: [true, false] }
+  validates :log_id, uniqueness: { scope: :question_id }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,7 @@
 class Question < ApplicationRecord
   has_many :answers
   has_many :logs, through: :answers
+
+  validates :content, presence: true
+  validates :is_active, inclusion: { in: [true, false] }
 end
