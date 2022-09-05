ac4dba941c00f2bf05201f739889ea5f777b2421
masanarih0ri <masanarihoridevelop@gmail.com>
2022-09-05 23:38:18 +0900

    modify settings for email of production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'sleeple-production.onrender.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_APP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -117,17 +130,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.delivery_method = :smtp
-  host = 'sleeple-production.onrender.com'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_APP_PASSWORD'],
-    authentication: :login,
-    enable_starttls_auto: true
-  }
 end
