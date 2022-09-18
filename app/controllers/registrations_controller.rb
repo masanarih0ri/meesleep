ef9bf29dcbcfffc09d48c1bc885e47fcf5243c7c
masanarih0ri <masanarihoridevelop@gmail.com>
2022-09-18 22:25:15 +0900

    modify send email anthentication page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(_resource)
-    new_user_session_path
+    account_authentications_sent_path
   end
 
   protected
